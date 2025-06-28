4aefa7eda70ad6ebcaf85d9c34d056c75876b8dd
ipilatova <irinapilatova@yandex.ru>
2025-06-28 23:45:19 +0300

    добавлены правки после ревью

--- task_2.py
+++ task_2.py
@@ -8,12 +8,12 @@ class Movies:
 
 class Comedy(Movies):
     def add_movie(self, movie):
-        self.movies.append(movie)
+        super().add_movie(movie)
         return f"Комедии: '{self.movies}'" # список взят в '', так как это требуется по заданию -  возвращать записи вида Комедии: '[]' и Драмы: '[]' соответственно)
 
 class Drama(Movies):
     def add_movie(self, movie):
-        self.movies.append(movie)
+        super().add_movie(movie)
         return f"Драмы: '{self.movies}'" # список взят в '', так как это требуется по заданию -  возвращать записи вида Комедии: '[]' и Драмы: '[]' соответственно)
 
 
